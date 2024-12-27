f248cab5c7ceea5916c24ab3c6a5e8b935f7f978
UoRoot <burnedprogrammer@gmail.com>
2024-12-27 14:05:37 -0500

    add another solution

--- leetcode/1342.Number-of-Steps-to-Reduce-a-Number-to-Zero/NumberOfStepsToReduceANumberToZero.java
+++ leetcode/1342.Number-of-Steps-to-Reduce-a-Number-to-Zero/NumberOfStepsToReduceANumberToZero.java
@@ -9,4 +9,14 @@ public class NumberOfStepsToReduceANumberToZero {
         }
         return steps;    
     }
+
+    public int numberOfSteps2(int num) {
+        if (num == 0) return 0;
+        int steps = 0;
+        while (num > 0) {
+            steps += (num & 1) == 1 ? 2 : 1;
+            num >>= 1;
+        }
+        return steps - 1;
+    }
 }

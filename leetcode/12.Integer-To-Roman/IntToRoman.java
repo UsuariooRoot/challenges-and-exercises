35bfda69a49954d8f39e1ccb7a64ee73c1b09de4
UoRoot <burnedprogrammer@gmail.com>
2025-01-04 10:14:33 -0500

    second solution to the int-to-roma exercise

--- leetcode/12.Integer-To-Roman/IntToRoman.java
+++ leetcode/12.Integer-To-Roman/IntToRoman.java
@@ -1,6 +1,9 @@
 public class IntToRoman {
+
+    private static String[][] roman = { { "I", "V" }, { "X", "L" }, { "C", "D" }, { "M", "" } };
+
     public static String intToRoman(int num) {
-        String[][] roman = { { "I", "V" }, { "X", "L" }, { "C", "D" }, { "M", "" } };
+        String[][] roman = new String[][] { { "I", "V" }, { "X", "L" }, { "C", "D" }, { "M", "" } };
 
         String[] digist = new String[4];
         int lastIdx = 3;
@@ -9,6 +12,7 @@ public class IntToRoman {
             int rep = unid % 5;
             if (unid == 5) {
                 digist[lastIdx] = roman[3 - lastIdx][1];
+                ;
             } else if (unid > 5) {
                 digist[lastIdx] = (rep <= 3)
                         ? roman[3 - lastIdx][1] + roman[3 - lastIdx][0].repeat(rep)
@@ -28,6 +32,36 @@ public class IntToRoman {
                 sb.append(d);
             }
         }
+        return sb.toString();
+    }
+
+    public static String intToRoman2(int num) {
+        StringBuilder sb = new StringBuilder();
+        int lastIdx = 0;
+        int div = 1000;
+        while (num > 0) {
+            int unid = num / div;
+            int rep = unid % 5;
+            if (unid > 0) {
+                int i = 3 - lastIdx;
+                sb.append(
+                        unid == 5
+                                ? roman[i][1]
+                                : unid > 5
+                                        ? rep == 4
+                                                ? roman[i][0] + roman[i + 1][0]
+                                                : roman[i][1] + roman[i][0].repeat(rep)
+                                        : rep == 4
+                                                ? roman[i][0] + roman[i][1]
+                                                : roman[i][0].repeat(rep)
+
+                );
+                num = num - (unid * div);
+            }
+            lastIdx++;
+            div /= 10;
+        }
+
         return sb.toString();
     }
 }

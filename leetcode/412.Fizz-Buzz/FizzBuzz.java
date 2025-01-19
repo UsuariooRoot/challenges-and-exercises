8e3024f737fbd5168d5e5795ed85edfcb643176c
UoRoot <burnedprogrammer@gmail.com>
2025-01-19 17:15:44 -0500

    clean solution of fizz-buzz excercise

--- leetcode/412.Fizz-Buzz/FizzBuzz.java
+++ leetcode/412.Fizz-Buzz/FizzBuzz.java
@@ -1,25 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Created: 2024-12-27
+ * Author: UoRoot
+ */
 public class FizzBuzz {
 
     public static List<String> fizzBuzz(int n) {
         List<String> answer = new ArrayList<>();
         for (int i = 1; i <= n; i++) {
-            String item = i % 3 == 0
-                    ? (i % 5 == 0 ? "FizzBuzz" : "Fizz")
-                    : i % 5 == 0
-                            ? "Buzz"
-                            : Integer.toString(i);
-            answer.add(item);
+            answer.add(
+                i % 15 == 0 ? "FizzBuzz"
+                : i % 3 == 0 ? "Fizz" 
+                : i % 5 == 0 ? "Buzz"
+                : Integer.toString(i)
+            );
         }
 
         return answer;
     }
 
-    public static void main(String[] args) {
-        System.out.println(fizzBuzz(3));
-        System.out.println(fizzBuzz(5));
-        System.out.println(fizzBuzz(15));
-    }
 }

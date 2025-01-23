fb9820c56e1e5965eb9024dc5930626f7d9e94e9
burntpro <burnedprogrammer@gmail.com>
2025-01-22 23:50:23 -0500

    add second solution for the problem 339A

--- codeforces/800/A/339/A_Helpful_Maths.java
+++ codeforces/800/A/339/A_Helpful_Maths.java
@@ -10,7 +10,7 @@ public class A_Helpful_Maths {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in);) {
             String input = scanner.next();
-            System.out.println(solve(input)); // You can choosing any so
+            System.out.println(solve2(input));
         }
     }
 
@@ -23,5 +23,63 @@ public class A_Helpful_Maths {
                 .map(String::valueOf)
                 .collect(Collectors.joining("+"));
         return output;
+    }
+
+    public static String solve2(String input) {
+        if (input.length() <= 2)
+            return input;
+        
+        StringBuilder sb = new StringBuilder(input);
+        dualPivotQuickSortAdapted(sb, 0, sb.length() - 1);
+        return sb.toString();
+    }
+
+    private static void dualPivotQuickSortAdapted(StringBuilder arr, int left, int right) {
+        if (right <= left) return;
+        
+        if (arr.charAt(left) > arr.charAt(right)) {
+            swap(arr, left, right);
+        }
+        
+        int pivot1 = arr.charAt(left) - 48;
+        int pivot2 = arr.charAt(right) - 48;
+        
+        int l = left + 2;
+        int k = l;
+        int g = right - 2;
+        
+        while (k <= g) {
+            if (arr.charAt(k) - 48 < pivot1) {
+                swap(arr, k, l);
+                l+=2;
+            } 
+            else if (arr.charAt(k) - 48 > pivot2) {
+                while (arr.charAt(g) - 48 > pivot2 && k < g) {
+                    g-=2;
+                }
+                swap(arr, k, g);
+                g-=2;
+                if (arr.charAt(k) - 48 < pivot1) {
+                    swap(arr, k, l);
+                    l+=2;
+                }
+            }
+            k+=2;
+        }
+        l-=2;
+        g+=2;
+        
+        swap(arr, left, l);
+        swap(arr, right, g);
+        
+        dualPivotQuickSortAdapted(arr, left, l - 2);
+        dualPivotQuickSortAdapted(arr, l + 2, g - 2);
+        dualPivotQuickSortAdapted(arr, g + 2, right);
+    }
+
+    private static void swap(StringBuilder arr, int i, int j) {
+        char temp = arr.charAt(i);
+        arr.setCharAt(i, arr.charAt(j));
+        arr.setCharAt(j, temp);
     }
 }

5897978fec8f07cf75e11e3ad0631c2f79f3c271
UoRoot <burnedprogrammer@gmail.com>
2025-01-25 16:49:42 -0500

    add solution of the merge-k-sorted-lists exercise (374ms)

---
+++ leetcode/23.Merge-K-Sorted-Linkedlist/merge-k-sorted-lists.ts
@@ -0,0 +1,34 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.next = (next === undefined ? null : next)
+    }
+}
+
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    const res = new ListNode(0);
+    let temp = res;
+
+    while (true) {
+        let lowestValueNode = Number.MAX_VALUE;
+        let idxLowestValueNode = -1;
+
+        for (let i = 0; i < lists.length; i++) {
+            if (!lists[i]) continue;
+            if ((lists[i] as ListNode).val < lowestValueNode) {
+                idxLowestValueNode = i;
+                lowestValueNode = (lists[i] as ListNode).val;
+            }
+        }
+
+        if (idxLowestValueNode < 0) break;
+
+        temp.next = lists[idxLowestValueNode] as ListNode;
+        temp = temp.next;
+        lists[idxLowestValueNode] = (lists[idxLowestValueNode] as ListNode).next;
+    }
+
+    return res.next;
+};

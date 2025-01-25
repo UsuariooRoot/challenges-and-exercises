6ebda9c0cab481abb93312cc5fa78eb416d8dba7
UoRoot <burnedprogrammer@gmail.com>
2025-01-25 18:06:29 -0500

    add second solution (132 ms)

--- leetcode/23.Merge-K-Sorted-Linkedlist/merge-k-sorted-lists.ts
+++ leetcode/23.Merge-K-Sorted-Linkedlist/merge-k-sorted-lists.ts
@@ -8,7 +8,7 @@ class ListNode {
 }
 
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
-    const res = new ListNode(-(10**4));
+    const res = new ListNode(-(10 ** 4));
     let temp = res;
 
     while (true) {
@@ -36,3 +36,37 @@ function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
 
     return res.next;
 };
+
+function mergeKLists2(lists: Array<ListNode | null>): ListNode | null {
+    if (lists.length < 1){
+        return null;
+    }
+
+    let list1 = lists[0];
+
+    for (let i = 1; i < lists.length; i++) {
+        list1 = mergeTwoLists(list1, lists[i])
+    }
+
+    return list1;
+};
+
+function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+    const res = new ListNode(0);
+    let temp = res;
+
+    while (list1 && list2) {
+        if (list1.val < list2.val) {
+            temp.next = list1;
+            list1 = list1.next;
+        } else {
+            temp.next = list2;
+            list2 = list2.next;
+        }
+        temp = temp.next;
+    }
+
+    temp.next = list1 == null ? list2 : list1;
+
+    return res.next;
+}

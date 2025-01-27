5a8b4916eb389a9853fb760e4c8f61dd7c5b066f
UoRoot <burnedprogrammer@gmail.com>
2025-01-26 20:19:12 -0500

    add third solution (2ms)

--- leetcode/1346.Check-If-N-And-Its-Double-Exist/CheckIfNAndItsDoubleExist.java
+++ leetcode/1346.Check-If-N-And-Its-Double-Exist/CheckIfNAndItsDoubleExist.java
@@ -41,4 +41,14 @@ public class CheckIfNAndItsDoubleExist {
         return false;
     }
 
+    public boolean checkIfExist3(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int doubled = arr[i];
+            for (int j = 0; j < arr.length; j++) {
+                if (i != j && doubled == arr[j]) return true;
+            }
+        }
+        return false;
+    }
+
 }

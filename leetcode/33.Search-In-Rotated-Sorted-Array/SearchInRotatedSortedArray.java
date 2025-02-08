9e1af5fc43ba7933c12e884514d43c5964cb5fa4
UoRoot <burnedprogrammer@gmail.com>
2025-02-08 18:27:58 -0500

    add first solution (0 ms)

---
+++ leetcode/33.Search-In-Rotated-Sorted-Array/SearchInRotatedSortedArray.java
@@ -0,0 +1,48 @@
+public class SearchInRotatedSortedArray {
+
+    public static int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        if (target < nums[left] && target > nums[right])
+            return -1;
+
+        if (nums[left] > nums[right]) {
+            while (left <= right) {
+                int middle = (left + right) / 2;
+
+                if (nums[left] >= nums[middle]) {
+                    left = middle;
+                    break;
+                }
+
+                left = middle + 1;
+            }
+
+            int idx = left;
+            while (nums[idx] < nums[0])
+                idx--;
+
+            if (target >= nums[0]) {
+                left = 0;
+                right = idx;
+            } else {
+                left = idx + 1;
+            }
+        }
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (target == nums[mid])
+                return mid;
+            if (nums[mid] > target)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+
+        return -1;
+    }
+
+}

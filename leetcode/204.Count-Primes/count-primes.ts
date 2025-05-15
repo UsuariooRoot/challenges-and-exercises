c4cdb05c472a7a0a707d197e4a7445275cb6f7e6
UsuariooRoot <burnedprogrammer@gmail.com>
2025-05-14 22:39:16 -0500

    add accepted solution (1189 ms)

--- leetcode/204.Count-Primes/count-primes.ts
+++ leetcode/204.Count-Primes/count-primes.ts
@@ -21,3 +21,28 @@ function countPrimes(n: number): number {
 
     return res;
 };
+
+function countPrimes2(n: number): number {
+    let res = 0;
+    if (n > 2) res++;
+    if (n > 3) res++;
+
+    function isPrime(n: number) {
+        if (n % 3 === 0)
+            return false;
+
+        for (let i = 5; i <= Math.sqrt(n); i += 6)
+            if (n % i === 0 || n % (i + 2) === 0)
+                return false;
+    
+        return true;
+    }
+
+    for (let num = 5; num < n; num += 2) {
+        if (isPrime(num)) {
+            res++;
+        }
+    }
+
+    return res;
+};
